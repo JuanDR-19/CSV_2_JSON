bc783fe42d71b1fbe3a1f435ee68912936ed2851
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-11-05 14:25:11 -0500

    "Ratings" class created

---
+++ src/main/java/org/example/Ratings.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.Getter;
+
+@Getter
+public class Ratings {
+    private int movieID;
+    private int rating;
+    private int timestamp;
+    private int UserID;
+
+    public Ratings(int movieID, int rating, int timestamp, int userID) {
+        this.movieID = movieID;
+        this.rating = rating;
+        this.timestamp = timestamp;
+        UserID = userID;
+    }
+
+}
